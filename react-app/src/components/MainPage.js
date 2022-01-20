37c31bdfccfbc6a14d4b55b7c2821df2e014375c
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-20 13:40:54 -0800

    heroku

--- react-app/src/components/MainPage.js
+++ react-app/src/components/MainPage.js
@@ -1,3 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom';
+import App from '../App';
 import LogoutButton from './auth/LogoutButton';
+
+const MainPage = () => {
+
+    return (
+        <h1> Taskimal </h1>
+    )
+}
+
+export default MainPage
