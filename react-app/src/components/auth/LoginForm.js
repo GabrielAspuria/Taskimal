34e2a9635fbd564c482f2c0d457652d699ba2211
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-20 10:37:21 -0800

    deploying to heroku

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -26,6 +26,11 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
+  const handleDemo = () => {
+    setEmail('demo@aa.io')
+    setPassword('password')
+  }
+
   if (user) {
     return <Redirect to='/' />;
   }
@@ -57,6 +62,7 @@ const LoginForm = () => {
           onChange={updatePassword}
         />
         <button type='submit'>Login</button>
+        <button onClick={handleDemo}>Demo User</button>
       </div>
     </form>
   );
