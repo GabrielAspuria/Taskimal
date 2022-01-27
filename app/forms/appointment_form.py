0ce9417523920585194d599bb41d891064248ab5
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-27 14:58:33 -0800

    a

--- app/forms/appointment_form.py
+++ app/forms/appointment_form.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import DateTimeField
+from wtforms import StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 class AddAppointmentForm(FlaskForm):
-    appointmentDate = DateTimeField('date', validators=[DataRequired()])
+    month = SelectField('month', choices=(('Jan'),('Feb'),('Mar'),('Apr'),('May'),('Jun'),('Jul'),('Aug'),('Sep'),('Oct'),('Nov'),('Dec')), validators=[DataRequired()])
+    day = IntegerField('day', validators=[DataRequired()])
+    time = SelectField('time', choices=(('1'), ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9'), ('10'), ('11'), ('12')), validators=[DataRequired()])
+    ap = SelectField('ap', choices=('AM', 'PM'), validators=[DataRequired()])
