b7bedabdd72051102513ca6ae49392b668278860
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-27 14:39:32 -0800

    appointments

--- app/api/appointment_routes.py
+++ app/api/appointment_routes.py
@@ -10,6 +10,4 @@ def index(userId):
     appointments = Appointment.get(userId)
     return {'appointments': [appointment.to_dict() for appointment in appointments]}
 
-@appointment_routes.route('/', methods=['POST'])
-@login_required
 
