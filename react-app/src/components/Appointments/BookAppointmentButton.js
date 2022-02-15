570cb61127397123040997c3270e637795d3b649
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-02-15 10:14:36 -0800

    error handling

--- react-app/src/components/Appointments/BookAppointmentButton.js
+++ react-app/src/components/Appointments/BookAppointmentButton.js
@@ -15,25 +15,24 @@ const BookAppointmentButton = (props) => {
     const [errors, setErrors] = useState([])
     const [month, setMonth] = useState(thisMonth)
     const [day, setDay] = useState('1')
-    const [time, setTime] = useState(1)
+    const [time, setTime] = useState('1')
     const [ap, setAp] = useState('AM')
-    console.log("MONTH", month)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const tooEarly = [12, 1, 2, 3, 4, 5, 6]
-        const tooLate = [10, 11]
         const thirtyMonths = ['Apr', 'Jun', 'Sep', 'Nov']
+        const tooEarly = ['12', '1', '2', '3', '4', '5', '6', '7']
+        const tooLate = ['9', '10', '11']
 
         const validationErrors = []
-        const dayRegex = /^[0-9]+(\.[0-9][0-9])?$/;
+        // const dayRegex = /^[0-9]+(\.[0-9][0-9])?$/;
         if(month === thisMonth && day < today && day > 0) validationErrors.push('Cannot schedule day in the past')
         if(day < 1 || day > 31) validationErrors.push('Please enter a valid day')
         if(thirtyMonths.includes(month) && day > 30) validationErrors.push('Please enter a valid day')
-        if(!dayRegex.test(day)) validationErrors.push('Please enter a numeric day')
-        if(month === 'Feb' && day > 28) validationErrors.push("February doesn't have that many days")
-        if(ap === 'AM' && tooEarly.includes(time)) validationErrors.push('Appointment is too early. Please choose a time between 8 AM and 10 PM')
-        if(ap === 'PM' && tooLate.includes(time)) validationErrors.push('Appointment is too late. Please choose a time between 8 AM and 10 PM')
+        // if(!dayRegex.test(day)) validationErrors.push('Please enter a numeric day')
+        if(month === 'Feb' && day > 28) validationErrors.push("Please enter a valid day")
+        if(ap === 'AM' && tooEarly.includes(time)) validationErrors.push('Please choose a time between 8 AM and 8 PM')
+        if(ap === 'PM' && tooLate.includes(time)) validationErrors.push('Please choose a time between 8 AM and 8 PM')
         setErrors(validationErrors)
 
         if(validationErrors.length === 0) {
@@ -87,7 +86,7 @@ const BookAppointmentButton = (props) => {
                             </select>
                             <input
                                 className="app-input"
-                                type='text'
+                                type='number'
                                 value={day}
                                 onChange={(e) => setDay(e.target.value)}
                             />
