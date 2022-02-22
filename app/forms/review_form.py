a159e0d46034fad1ffd97abedd29c93dc7fc0be1
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-02-22 09:27:24 -0800

    made review model

---
+++ app/forms/review_form.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import TextAreaField, SelectField
--- app/models/review.py
+++ app/models/review.py
@@ -2,3 +2,21 @@ from .db import db
 
 class Review(db.Model):
     __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey=('users.id'))
+    taskId = db.Column(db.Integer, db.ForeignKey=('tasks.id'))
+    review = db.Column(db.String(300), nullable=False)
+    rating = db.Column(db.Integer(), nullable=False)
+
+    user = db.relationship('User', back_populates='review')
+    task = db.relationship('Task', back_populates='review')
+
+    def to_dict(self):
+        return {
+            'id' = self.id,
+            'userId' = self.userId,
+            'taskId' = self.taskId,
+            'review' = self.review,
+            'rating' = self.rating
+        }
