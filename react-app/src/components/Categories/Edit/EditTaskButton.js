2e39681732bf286dc5a9cb202426a83c9d39d40a
GabrielAspuria <gabrielmaspuria@gmail.com>
2022-03-23 11:26:21 -0700

    edit buttons

--- react-app/src/components/Categories/Edit/EditTaskButton.js
+++ react-app/src/components/Categories/Edit/EditTaskButton.js
@@ -16,6 +16,18 @@ const EditTaskButton = (props) => {
     const [editPictures, setPictures] = useState(props.task?.pictures)
     const { id } = useParams();
 
+    const [check, setCheck] = useState(true)
+    let change = async (e) => {
+        if(check === true) {
+            console.log(check)
+            setCheck(false)
+        } else {
+            console.log(check)
+            setCheck(true)
+            
+        }
+    }
+
     const handleDelete = async (e) => {
         e.preventDefault()
         const confirmed = window.confirm('Delete task?')
@@ -57,97 +69,107 @@ const EditTaskButton = (props) => {
     }
 
     return (
-        <form onSubmit={handleEdit} className='edit-task-form'>
-            <h2> Edit Task </h2>
+        <div>
+            <div className='edit-task-button-div'>
+                <button onClick={change} className='edit-task-button'>Edit Task</button>
+            </div>
             <div>
-                <div>
-                    <ul>
-                        {errors.length > 0 &&
-                        errors.map(error => (
-                            <li key={error}> {error} </li>
-                        ))}
-                    </ul>
-                </div>
 
-            <div className='add-task-input'>
-                <div><label> Animal </label></div>
-                {/* <select
-                value={editAnimal}
-                onChange={(e) => setAnimal(e.target.value)}
-                >
-                    <option value='Any'> Any </option>
-                    <option value='Dog'> Dog </option>
-                    <option value='Cat'> Cat </option>
-                    <option value='Bird'> Bird </option>
-                    <option value='Reptile'> Reptile </option>
-                    <option value='Misc'> Misc </option>
-                </select> */}
-                <input
-                            placeholder='What kind of animal(s)?'
-                            type='text'
+                {check === false && (
+                    <form onSubmit={handleEdit} className='edit-task-form'>
+                        <h2> Edit Task </h2>
+                        <div>
+                            <div>
+                                <ul>
+                                    {errors.length > 0 &&
+                                    errors.map(error => (
+                                        <li key={error}> {error} </li>
+                                    ))}
+                                </ul>
+                            </div>
+            
+                        <div className='add-task-input'>
+                            <div><label> Animal </label></div>
+                            {/* <select
                             value={editAnimal}
-                            onChange={e => setAnimal(e.target.value)}
-                            maxLength='50'
-                            onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
-                        ></input>
+                            onChange={(e) => setAnimal(e.target.value)}
+                            >
+                                <option value='Any'> Any </option>
+                                <option value='Dog'> Dog </option>
+                                <option value='Cat'> Cat </option>
+                                <option value='Bird'> Bird </option>
+                                <option value='Reptile'> Reptile </option>
+                                <option value='Misc'> Misc </option>
+                            </select> */}
+                            <input
+                                        placeholder='What kind of animal(s)?'
+                                        type='text'
+                                        value={editAnimal}
+                                        onChange={e => setAnimal(e.target.value)}
+                                        maxLength='50'
+                                        onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
+                                    ></input>
+                        </div>
+            
+                        </div>
+            
+                        <div className='add-task-input'>
+                            <div>
+                                <div><label> Name </label></div>
+                                <input
+                                    type='text'
+                                    value={editName}
+                                    onChange={e => setName(e.target.value)}
+                                    maxLength='50'
+                                    onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
+                                />
+                            </div>
+                        </div>
+            
+                        <div className='add-task-input'>
+                            <div><label> Price: </label></div>
+                            <input
+                                type='number'
+                                value={editPrice}
+                                onChange={e => setPrice(e.target.value)}
+                                onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
+                            />
+                        </div>
+            
+                        <div className='add-task-input'>
+                            <div><label> Picture </label></div>
+                            <input
+                                type='text'
+                                value={editPictures}
+                                onChange={e => setPictures(e.target.value)}
+                                placeholder='JPG, PNG, or GIF'
+                                onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
+                            />
+                        </div>
+            
+                        <div className='add-task-input'>
+                            <div><label> Description </label></div>
+                            <textarea
+                                type='text'
+                                value={editDescription}
+                                onChange={e => setDescription(e.target.value)}
+                                maxLength='500'
+                            />
+                        </div>
+                            <div className='edit-buttons'>
+                            <button
+                                className='delete-task-button'
+                                onClick={handleDelete}
+                                id={id}
+                                >
+                                Delete
+                            </button>
+                            <button className='submit-edit-button'>Submit</button>
+                            </div>
+                    </form>
+                )}
             </div>
-
-            </div>
-
-            <div className='add-task-input'>
-                <div>
-                    <div><label> Name </label></div>
-                    <input
-                        type='text'
-                        value={editName}
-                        onChange={e => setName(e.target.value)}
-                        maxLength='50'
-                        onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
-                    />
-                </div>
-            </div>
-
-            <div className='add-task-input'>
-                <div><label> Price: </label></div>
-                <input
-                    type='number'
-                    value={editPrice}
-                    onChange={e => setPrice(e.target.value)}
-                    onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
-                />
-            </div>
-
-            <div className='add-task-input'>
-                <div><label> Picture </label></div>
-                <input
-                    type='text'
-                    value={editPictures}
-                    onChange={e => setPictures(e.target.value)}
-                    placeholder='JPG, PNG, or GIF'
-                    onKeyPress={(e) => {e.key === 'Enter' && e.preventDefault()}}
-                />
-            </div>
-
-            <div className='add-task-input'>
-                <div><label> Description </label></div>
-                <textarea
-                    type='text'
-                    value={editDescription}
-                    onChange={e => setDescription(e.target.value)}
-                    maxLength='500'
-                />
-            </div>
-                <div className='edit-buttons'>
-                <button
-                    className='delete-task-button'
-                    onClick={handleDelete}
-                    id={id}
-                    >
-                    Delete
-                </button>
-                <button className='submit-edit-button'>Submit</button>
-                </div>
-        </form>
+        </div>  
     )
 }
 
