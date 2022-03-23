ec39dd754127942d44dd38494f573f8090ac73ab
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-03-22 17:23:59 -0700

    review seeders

---
+++ app/seeds/review.py
@@ -0,0 +1,32 @@
+from app.models.review import db, Review
+
+def seed_reviews():
+    demo_review_one = Review(
+        userId=1,
+        taskId=1,
+        rating=5,
+        review='Fantastic walker!'
+    )
+
+    demo_review_two = Review(
+        userId=2,
+        taskId=1,
+        rating=5,
+        review='Always go to Marnie for walks!'
+    )
+
+    demo_review_three = Review(
+        userId=1,
+        taskId=2,
+        rating=5,
+        review='Fantastic crate trainer!'
+    )
+
+    db.session.add(demo_review_one)
+    db.session.add(demo_review_two)
+    db.session.add(demo_review_three)
+    db.session.commit()
+
+def undo_reviews():
+    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+    db.session.commit()
